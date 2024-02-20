44f225eb8bfe7e55367201c19a81f7a006c9ec44
Aaron Myatt <aaronmyatt@gmail.com>
2024-02-20 23:36:06 +0800

    update public path

--- quasar.config.js
+++ quasar.config.js
@@ -71,7 +71,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: process.env.PROD ? '/rewardmeweb/' : '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
